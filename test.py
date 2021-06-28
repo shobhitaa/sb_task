de98c4b144e39f1b5fb7b6414b51c4b5a781ee4d
shobhitaa <shobha2738@gmail.com>
2021-06-28 10:53:17 +0530

    create matrix map for dp

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ from collections import OrderedDict
 fee = OrderedDict()
 weight = OrderedDict()
 parents = OrderedDict()
+matrix_map = OrderedDict()
 matrix_dictionary = OrderedDict()
+sorted_weights = []
 block_weight = 4000000
 no_of_txid = 0
 matrix =[]
@@ -33,6 +35,12 @@ def calculateTotalWeight():
 
 def sortWeights():
     sorted_weights = sorted(weight.items(), key=lambda x: x[1])
+    #j = 1
+    #print(sorted_weights[0][1])
+    # for i in list(sorted_weights.keys()):
+    #     sorted_ids[i] = j
+    #     j += 1
+    # print(sorted_ids)
     # c = 1
     # for i in sorted_weights:
     #     print(c, i[0], i[1])
@@ -40,9 +48,15 @@ def sortWeights():
 
 def dp():
     temp_list = []
+    sorted_weights = sorted(weight.items(), key=lambda x: x[1])
     for i in range(0, block_weight + 1):
         temp_list.append(0)
     matrix.append(temp_list)
+
+    for i in range(0, 2):
+        for j in range(0, 2):
+            str1 = 'matrix[' + sorted_weights[i][0] + "][" + str(j) + "]"
+            matrix_map[str1] = []
 
 
 calculateTotalWeight()
